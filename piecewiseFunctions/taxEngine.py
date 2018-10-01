4f24fb5233e3df05ddcae43dd23c3b710b5fe175
Ethan Vazquez <edv121@outlook.com>
2018-10-01 16:02:13 -0700

    moved to single file

--- piecewiseFunctions/taxEngine.py
+++
@@ -1,7 +0,0 @@
-def taxRateFinder(taxPer,bracket):
-    taxRate = []
-    for number in range(len(bracket)) :
-      taxRate.append((bracket[number] - bracket[number - 1]) * taxPer[number])
-      if taxRate[number] != 0 :
-        taxRate[number] = taxRate[number] + taxRate[number - 1]
-    return taxRate
