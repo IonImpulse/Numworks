bcb8b89f781e8808b260c008bd8c24db6e102a9a
Ethan Vazquez <edv121@outlook.com>
2018-10-08 08:59:39 -0700

    fixed it all

--- regressions/regressions.py
+++ regressions/regressions.py
@@ -1,3 +1,4 @@
+import math
 def linTable (inputList,outputList) :
     outputList.append(inputList[:len(inputList)//2])
     if len(inputList)% 2 == 0 :
@@ -18,9 +19,23 @@ xAvgLow = sum(xList1[0])/float(len(xList1[0]))
 xAvgHigh = sum(xList1[1])/float(len(xList1[1]))
 yAvgLow = sum(yList1[0])/float(len(yList1[0]))
 yAvgHigh = sum(yList1[1])/float(len(yList1[1]))
-xAvg = (xAvgLow + xAvgHigh) / 2
-yAvg = (yAvgLow + yAvgHigh) / 2
-slope = float(yAvg/xAvg)
-x = 1
-yAvg = 0 - yAvg
-b = yAvg/(slope * (0 - xAvg))
+slope = float((yAvgHigh-yAvgLow)/(xAvgHigh-xAvgLow))
+b = yAvgLow - (xAvgLow * slope)
+print("Linear:")
+print("y =",slope,"x +",b)
+print("============")
+yQuadList = [x+(0-yList[0]) for x in yList]
+xQuadList = [x+(0-xList[0]) for x in xList]
+yQuadList1 = []
+xQuadList1 = []
+linTable(yQuadList,yQuadList1)
+linTable(xQuadList,xQuadList1)
+if len(yQuadList)% 2 == 0 :
+    tMiddle = int(len(yQuadList) / 2)
+    yMiddle = float((yQuadList[tMiddle]) + .5)
+else :
+    tMiddle = int((len(yQuadList) + 1) / 2)
+    yMiddle = float(yQuadList[tMiddle])
+xMiddle = math.sqrt(xQuadList1[1][len(xQuadList1[1])-1])
+print(xQuadList1)
+print(xMiddle)
